a691e8fd99656443db43e7a7f92bac34e89269d5
Candace Savonen <cansav09@gmail.com>
2020-06-01 16:44:58 -0400

    Set up set up script better

--- 00-data_download.sh
+++ 00-data_download.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# J. Taroni 2018
 
-# set up directories
+# C. Savonen 2020
+
+# Set up directories
 mkdir data && mkdir plots && mkdir results && mkdir util
 
-# data directory subdirectories
-cd data && mkdir expression_data
+# Data directory subdirectories
+cd data && mkdir data/expression_data
 
 # get recount2 data & model from figshare, source code in 
 # greenelab/rheum-data-plier
 wget https://ndownloader.figshare.com/files/10881866 \
-  -O recount2.zip
-unzip recount2.zip && rm recount2.zip
+  -O data/recount2.zip
+unzip data/recount2.zip && rm data/recount2.zip
 
-## microarray data from greenelab/rheum-plier-data
-cd expression_data
+# Copy scripts from multiplier repo
+wget https://github.com/greenelab/multi-plier/raw/9b8719ab3d753a29e0788831d2461b0bfd040783/util/plier_util.R \
+-O util/plier_util.R
+
+wget https://github.com/greenelab/multi-plier/raw/9b8719ab3d753a29e0788831d2461b0bfd040783/util/test_LV_differences.R \
+-O util/test_LV_differences.R
